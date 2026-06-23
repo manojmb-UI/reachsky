ffb3366839747becd0e338b3284fe5a4807e8238
Manoj Balamurugan <manoj.balamurugan@impigertech.com>
2026-06-24 00:46:58 +0530

    6th  commit

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -5,16 +5,10 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = (
-   "mysql://root:KKjVQigeAivtYMNdWYwrKgBXduVfguYk@reseau.proxy.rlwy.net:33443/railway"
-)
+DATABASE_URL = "mysql+pymysql://root:KKjVQigeAivtYMNdWYwrKgBXduVfguYk@reseau.proxy.rlwy.net:33443/railway"
 
 engine = create_engine(DATABASE_URL)
 
-print("DB_HOST =", os.getenv("DB_HOST"))
-print("DB_PORT =", os.getenv("DB_PORT"))
-print("DB_USER =", os.getenv("DB_USER"))
-print("DB_NAME =", os.getenv("DB_NAME"))
 
 SessionLocal = sessionmaker(
     autocommit=False,
