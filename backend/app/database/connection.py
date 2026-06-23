aceb78d1f362d9cee5eb2f8af90ab905c5525a2d
Manoj Balamurugan <manoj.balamurugan@impigertech.com>
2026-06-24 00:09:21 +0530

    third commit

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -15,6 +15,11 @@ DATABASE_URL = (
 
 engine = create_engine(DATABASE_URL)
 
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_PORT =", os.getenv("DB_PORT"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_NAME =", os.getenv("DB_NAME"))
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
